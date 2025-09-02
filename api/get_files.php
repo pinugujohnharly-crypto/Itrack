e01c9926ec7078e6cb0b0097b6b6fc062bb31eb3
pinugujohnharly-crypto <pinugujohnharly@gmail.com>
2025-09-03 03:21:21 +0800

    Update get_files.php

--- api/get_files.php
+++ api/get_files.php
@@ -3,12 +3,15 @@ include '../database.php';
 
 header('Content-Type: application/json');
 
-$sql = "SELECT * FROM uploaded_files WHERE approved = 1 ORDER BY uploaded_at DESC";
+$sql = "SELECT id, filename, url, uploaded_by, capstone_title, uploaded_at, approved ,date_uploaded
+        FROM uploaded_files 
+        WHERE approved = 1 
+        ORDER BY uploaded_at DESC";
 $result = $conn->query($sql);
 
 $files = [];
 
-if ($result && $result->num_rows > 1) {
+if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $files[] = $row;
     }
