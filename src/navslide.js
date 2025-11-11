55206cf4b2d0b7cc82580dd46fa46618bfe1233d
pinugujohnharly-crypto <pinugujohnharly@gmail.com>
2025-11-11 21:35:23 +0800

    adjusting nav slide and design

--- src/navslide.js
+++ src/navslide.js
@@ -1,12 +1,16 @@
 const navToggle = document.getElementById("navToggle");
 const sidebar = document.getElementById("sidebar");
 const navslide = document.querySelector(".navslide");
+const contentContainer = document.querySelector(".content-container");
+const pager = document.querySelector(".pager");
 
-// Start closed
 sidebar.classList.add("closed");
 navslide.classList.add("closed");
 
 navToggle.addEventListener("click", () => {
-  sidebar.classList.toggle("closed");
+  sidebar.classList.toggle('open');
   navslide.classList.toggle("closed");
+  document.body.classList.toggle("sidebar-open"); // 👈 add this
+  document.body.classList.toggle("no-scroll"); // 👈 add this
 });
+
